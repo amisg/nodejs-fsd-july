075223d98e5c01e74a0749b5b760e50a38e9b23b
amisg <aamithsg18@gmail.com>
2024-09-30 16:17:58 +0530

    basics

---
+++ 1.Basics/index.js
@@ -0,0 +1,16 @@
+// console.log("hello world");
+
+// function abc(params) {
+// 	return 1;
+// }
+
+// let ans = abc();
+// console.log(ans);
+
+console.log("a");
+
+setTimeout(() => {
+	console.log("b");
+}, 0);
+
+console.log("c");
